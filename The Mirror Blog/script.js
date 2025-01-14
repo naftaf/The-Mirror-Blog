e05998b0aa9a53c7973591f98335a86f9718ec91
naftaf <nafisap@nycstudents.net>
2025-01-14 10:21:37 -0500

    Set up firebase

---
+++ The Mirror Blog/script.js
@@ -0,0 +1,42 @@
+/* === Imports === */
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import {createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { signIsnWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+
+  
+/* === FIREBASE SETUP === */
+const firebaseConfig = {
+apiKey: "AIzaSyDM-jfigZF6W3n-igzKPWxwZq2JEcaIQL4",
+authDomain: "the-mirror-blog.firebaseapp.com",
+databaseURL: "https://the-mirror-blog-default-rtdb.firebaseio.com",
+projectId: "the-mirror-blog",
+storageBucket: "the-mirror-blog.firebasestorage.app",
+messagingSenderId: "876746537077",
+appId: "1:876746537077:web:125ffe67b7a761d7be603c",
+measurementId: "G-6PB4583T6T"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+/* === UI === */
+
+
+/* == UI - Elements == */
+
+
+/* == UI - Event Listeners == */
+
+
+/* === Main Code === */
+
+
+/* === Functions === */
+
+
+/* = Functions - Firebase - Authentication = */
+
+
+/* == Functions - UI Functions == */
