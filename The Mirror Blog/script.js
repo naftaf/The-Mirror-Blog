19c54a559ef217580b958c34bef6c84d0c1d18ec
naftaf <nafisap@nycstudents.net>
2025-01-14 10:42:44 -0500

    Added more Firebase functions

--- The Mirror Blog/script.js
+++ The Mirror Blog/script.js
@@ -24,12 +24,88 @@ const analytics = getAnalytics(app);
 
 
 /* == UI - Elements == */
-
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("sign-up-btn")
+const signOutButtonEl = document.getElementById("sign-out-btn")
+const userProfilePictureEl = document.getElementById("user-profile-picture")
+const userGreetingEl = document.getElementById("user-greeting")
 
 /* == UI - Event Listeners == */
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+signOutButtonEl.addEventListener("click", authSignOut)
 
 
 /* === Main Code === */
+authFunction();
+
+function authFunction() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        showLoggedInView();
+        const uid = user.uid;
+        showProfilePicture(userProfilePictureEl, user);
+        showUserGreeting(userGreetingEl, user);
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        showLoggedOutView();
+      }
+    });
+}
+
+async function showProfilePicture(imgElement, user) {
+    const auth = getAuth();
+    if (user !== null) {
+        // The user object has basic properties such as display name, email, etc.
+        const displayName = user.displayName;
+        const email = user.email;
+        const pfp = await displayImage();
+        imgElement.src = URL.createObjectURL(pfp)
+        const emailVerified = user.emailVerified;
+    
+        // The user's ID, unique to the Firebase project. Do NOT use
+        // this value to authenticate with your backend server, if
+        // you have one. Use User.getToken() instead.
+        const uid = user.uid;
+    }
+ }
+
+ async function displayImage() { 
+    const response = await fetch('https://unsplash.it/1920/1080?random'); 
+    const blob = await response.blob(); 
+    return blob; 
+ }
+ 
+ function showUserGreeting(element, user) {
+        const auth = getAuth();
+        if (user !== null) {
+            console.log(user.displayName);
+          // The user object has basic properties such as display name, email, etc.
+          if (user.displayName){
+            element.textContent = "Hi " + user.displayName;
+          }
+          else {
+            element.textContent = "Hey friend, how are you?";
+          }
+          const email = user.email;
+          const photoURL = user.photoURL;
+          const emailVerified = user.emailVerified;
+        
+          // The user's ID, unique to the Firebase project. Do NOT use
+          // this value to authenticate with your backend server, if
+          // you have one. Use User.getToken() instead.
+          const uid = user.uid;
+        }
+ }
 
 
 /* === Functions === */
@@ -69,7 +145,35 @@ function authCreateAccountWithEmail() {
     });
 }
 
-
-/* = Functions - Firebase - Authentication = */
+function authSignOut() {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        showLoggedOutView();
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+    });
+}
 
 /* == Functions - UI Functions == */
+function showLoggedOutView() {
+hideView(viewLoggedIn)
+showView(viewLoggedOut)
+}
+
+
+function showLoggedInView() {
+hideView(viewLoggedOut)
+showView(viewLoggedIn)
+}
+
+
+function showView(view) {
+view.style.display = "flex"
+}
+
+
+function hideView(view) {
+view.style.display = "none"
+}
